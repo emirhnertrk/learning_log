0716602d5cc7ce1f4d732b571c2361051d589608
emirhnertrk <142717211+emirhnertrk@users.noreply.github.com>
2024-01-06 14:15:03 +0300

    1. Django projesi oluşturma 2. -venv oluşturma ve aktif etme 3.uygulama(app) oluşturma 4.Model tanımlama(models.py) 5.admin sitesi yapılandırma 6.url tanımlama(urls.py) 7.şablon (templates) oluşturma  -HTML  -şablon etiketler {%...%} 8. Migration yapma 9. Ayarları Dosyası (settings.py) 10.View fonksiyonu yazma (views.py)

---
+++ learning_log2/learning_logs/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+    """A topic the user is interested in."""
+    # text ve date_added adında veri tabanında 2 sütun oluşturacağız
+    opjects = None
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the topic."""
+        return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    # ForeignKey field'ı "çoktan bire" ilişkiyi temsil eder. Her Entry bir Topic ile ilişkilidir.
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Entries"
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return f"{self.text[:50]}..."
+
+
